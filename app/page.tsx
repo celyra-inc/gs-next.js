7b7b997d1a6c7491ee08a7429af410170993bace
AlphaSagittarii <mg.esec@intelligence-airbusds.de>
2024-09-28 17:57:29 +0200

    added per-user data isolation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { ThemeProvider } from "@aws-amplify/ui-react";
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
@@ -10,6 +10,7 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { TodoCreateForm } from '../ui-components';
 
 Amplify.configure(outputs);
 
@@ -39,26 +40,29 @@ export default function App() {
   }
 
   return (
-        
-    <Authenticator>
-      {({ signOut, user }) => (
-      <main>
-        <h1>Roadmap</h1>
-        <button onClick={createTodo}>+ new</button>
-        <ul>
-          {todos.map((todo) => (
-            <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
-              {todo.content}
-            </li>
-          ))}
-        </ul>
-        <div>
-          🥳 App successfully hosted. Try creating a new todo.
-          <br />
-        </div>
-        <button onClick={signOut}>Sign out</button>
-      </main>
-      )}
-    </Authenticator>
+    <ThemeProvider>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <TodoCreateForm />
+            <h1>{user?.signInDetails?.loginId}'s todos</h1>
+            <h1>Roadmap</h1>
+            <button onClick={createTodo}>+ new</button>
+            <ul>
+              {todos.map((todo) => (
+                <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
+                  {todo.content}
+                </li>
+              ))}
+            </ul>
+            <div>
+              🥳 App successfully hosted. Try creating a new todo.
+              <br />
+            </div>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+    </ThemeProvider>
   );
-} 
+}
